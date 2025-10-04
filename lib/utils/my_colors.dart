29eb9017644dcebd1667bdfea8c5bfe19e84c29f
yusmnn <yusmnn@gmail.com>
2025-10-05 00:08:11 +0800

    feat: create component colors

---
+++ lib/utils/my_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF0EBE7F);
+const Color secondaryColor = Color(0xFF677294);
+const Color backgroundColor = Color(0xFFFFFFFF);
+const Color textColor = Color(0xFF333333);
+
+// home button
+const Color darkBlue = Color(0XFF2753F3);
+const Color purple = Color(0XFF765AFC);
+const Color orange = Color(0XFFFE7F44);
+const Color yellow = Color(0XFFFFCF68);
+const Color red = Color(0XFFFFCF68);
+const Color pastelRed = Color(0XFFFF6C60);
