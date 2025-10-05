f7f3854895a9327fcd615a8e278cf15fdbd35fa2
yusmnn <yusmnn@gmail.com>
2025-10-05 18:47:39 +0800

    feat: onboarding ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:doctor_hunt/view/pages/home/home_page.dart';
+import 'package:doctor_hunt/view/pages/onboarding/onboarding_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,56 +14,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Doctor Hunt',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
       ),
-      home: const MyHomePage(title: 'Doctor Hunt Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: OnboardingPage(),
+      routes: {HomePage.routeName: (context) => const HomePage()},
     );
   }
 }
